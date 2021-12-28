d032a6f985b3f5b973a96d1df4694590f45c4c73
Artur Aliakseyeu <aae@anahorets-iMac.local>
2021-12-28 17:30:15 +0300

    sdfdsf

--- app/javascript/components/Product.js
+++ app/javascript/components/Product.js
@@ -4,7 +4,7 @@ import { Changer } from "./Changer"
 
 const Product = ({ product }) => {
 	const dispatch = useDispatch()
-	
+
 	return (
 		<div className="card" style={{ width: '18rem' }}>
 			<img
@@ -14,7 +14,7 @@ const Product = ({ product }) => {
 				alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">{product.name}</h5>
-				<p className="card-text">{product.price} $</p>
+				<p className="card-text">{product.price * product.count} $</p>
 				<Changer id={product.id} count={product.count} />
 				<button onClick={() =>
 					dispatch({ type: 'add', payload: product.id })}

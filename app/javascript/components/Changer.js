8a1ab75b0b16d22d22f45ab233f961507cfe4ce7
Artur Aliakseyeu <aae@anahorets-iMac.local>
2022-01-17 09:18:33 +0300

    fx

--- app/javascript/components/Changer.js
+++ app/javascript/components/Changer.js
@@ -5,30 +5,30 @@ import { useDispatch } from 'react-redux'
 export const Changer = ({ id, count }) => {
   const dispatch = useDispatch()
 
-  const changeCount = (e) => {
-    if (e.target.value === '+') {
-      dispatch({ type: 'setCount', payload: { id: id, count: count + 1 } })
-    } else if (e.target.value === '-') {
-      if (count - 1 <= 0) return dispatch({ type: 'setCount', payload: { id: id, count: 1 } })
-      dispatch({ type: 'setCount', payload: { id: id, count: count - 1 } })
-    } else {
-      if (e.target.value <= 0) return dispatch({ type: 'setCount', payload: { id: id, count: 1 } })
-      dispatch({ type: 'setCount', payload: { id: id, count: e.target.value } })
-    }
+  const minusHandle = () => {
+    if (count - 1 <= 0) return dispatch({ type: 'setCount', payload: { id: id, count: 1 } })
+    dispatch({ type: 'setCount', payload: { id: id, count: count - 1 } })
+  }
+
+  const plusHandle = () => {
+    dispatch({ type: 'setCount', payload: { id: id, count: count + 1 } })
+  }
+
+  const inputHandle = e => {
+    if (e.target.value <= 0) return dispatch({ type: 'setCount', payload: { id: id, count: 1 } })
+    dispatch({ type: 'setCount', payload: { id: id, count: e.target.value } })
   }
 
   return (
     <div className="col">
-      <button onClick={changeCount} value='-'>-</button>
+      <button onClick={minusHandle} value='-'>-</button>
       <input
-        type='text'
-        pattern='\d*'
-        maxLength='5'
-        onChange={changeCount}
+        type='number'
+        onChange={inputHandle}
         style={{ width: '50px' }}
         value={count}
       />
-      <button onClick={changeCount} value='+'>+</button>
+      <button onClick={plusHandle} value='+'>+</button>
     </div>
   )
 }
---
+++ app/javascript/tests/products.test.js
@@ -0,0 +1,9 @@
+import {Products} from '../utilities'
+
+test('we can import ititial product list ', () => {
+    expect(Products).toBeTruthy();
+});
+
+test('Initital product list has all data that we need ', () => {
+    expect(Products).toBeTruthy();
+});
